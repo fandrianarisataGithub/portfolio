7631554d50072f11d7ae9eea0af58f0730bd5d1a
Fenitriniaina <git.fandrianarisata@gmail.com>
2024-01-18 13:14:57 +0300

    separate vite.config for command serve and build

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/*export default defineConfig({
   plugins: [
     vue(),
   ],
@@ -17,5 +17,33 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base : "/"
+  base: mode === 'production' ? '/portfolio/' : '/',
+})*/
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      base: '/'
+    }
+  } else {
+    // command === 'build'
+    return {
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      base: '/portfolio'
+    }
+  }
 })
